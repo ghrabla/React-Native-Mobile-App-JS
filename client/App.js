d3b0f6ad4856bededb0fe761229687a2a49e3dc9
Kamal Rhrabla <kamalghrabla@gmail.com>
2023-01-28 21:40:41 +0100

    finish login page

--- client/App.js
+++ client/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { View, Text, StyleSheet, TextInput} from 'react-native';
+import { View, Text, StyleSheet, TextInput, Pressable} from 'react-native';
 
 
 export default function App() {
@@ -8,8 +8,12 @@ export default function App() {
     <View style={styles.container}>
       <Text style={styles.text1}>Welcom Again!</Text>
       <Text style={styles.text2}>welcom back you have been missed!</Text>
-      <TextInput style={styles.input} placeholder="Type email her"></TextInput>
-      <TextInput style={styles.input} placeholder="Type password here"></TextInput>
+      <TextInput style={styles.input} placeholder="Type email here"></TextInput>
+      <TextInput style={styles.input} secureTextEntry={true} placeholder="Type password here"></TextInput>
+      <Text style={styles.text4}>you don't have an account ? Register</Text>
+      <Pressable style={styles.button} >
+      <Text style={styles.text3}>Login</Text>
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
@@ -33,6 +37,17 @@ const styles = StyleSheet.create({
     fontSize: 20,
     margin: 12
   },
+  text3: {
+    color: 'white',
+    fontWeight: "bold",
+    textAlign: 'center',
+  },
+  text4:{
+    color: "black",
+    fontSize: 13,
+    marginLeft: 70,
+    marginTop: 20
+  },
   input:{
     width: 300,
     height: 40,
@@ -44,5 +59,16 @@ const styles = StyleSheet.create({
     borderRadius: 10, 
     fontSize: 16,
     marginTop: 20
+  },
+  button:{
+    marginTop: 20,
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    backgroundColor: 'black',
+    width: 300,
+    height: 40,
+    
   }
+
 });
